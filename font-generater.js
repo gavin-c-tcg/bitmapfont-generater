bcb447b12ac5573382bcd1cd80cd82d3aea49e28
gavin ted <youremail@yourdomain.com>
2023-03-03 17:39:46 +0800

    調整

---
+++ font-generater.js
@@ -0,0 +1,41 @@
+const generator = require("./src/index");
+
+exports.genRobotoThinItalic = async () => {
+  let resolution = 4;
+  let fontSize = 28;
+  await generator.TextStyle2BitmapFont({
+    path: "./output/Roboto-ThinItalic",
+    fileName: `BBwit`,
+    textSet: "sadfsdfsdgSDDDD", //Phaser.GameObjects.RetroFont.TEXT_SET1,
+    textStyle: {
+      fontFamily: "Roboto-ThinItalic",
+      fontSize: `${resolution * fontSize}px`,
+      align: "center",
+      stroke: "#000",
+      strokeThickness: resolution,
+      color: "#ffffff",
+      shadow: {
+        offsetX: 0.5 * resolution,
+        offsetY: 0.5 * resolution,
+        blur: 0,
+        fill: true,
+        stroke: true,
+        color: "#000000",
+      },
+    },
+  });
+};
+
+exports.genTimeNumber = async () => {
+  await generator.TextStyle2BitmapFont({
+    path: "./output/time-number",
+    fileName: `TimeNumber`,
+    images: {
+      fontDir: "./images/time-number",
+      fontFamily: "TimeNumber",
+      // lineHeight: 116,
+      // base: 116,
+      // fontSize: 72
+    },
+  });
+};
--- readme.md
+++ readme.md
@@ -1,10 +1,17 @@
 # Phaser bitmap font generator
 
+## start
+
+```bash
+	# 修改 generate-bitmapfont.js 中的 function ， 一次只可產生個字型 需要分次執行
+	yarn start
+```
+
 ## 安裝
 
 - 安裝失敗 https://github.com/Automattic/node-canvas/issues/1065
 
-### Windows, Docker
+### Windows or Docker
 
 ```bash
 	yarn docker:build
@@ -14,6 +21,14 @@
 ```
 
 ### MAC 不推薦
+
+需要安裝 xcode command line tools
+
+```bash
+	xcode-select --install
+```
+
+安裝 node-canvas 依賴工具
 
 ```bash
 	brew install pkgconfig
